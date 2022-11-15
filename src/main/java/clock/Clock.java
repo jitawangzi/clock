9135791fc6cc74247fea7d7cbba74f4a99352192
shuyongqiang <shuyongqiang7704@dingtalk.com>
2022-11-15 14:50:30 +0800

    整理

--- src/main/java/clock/Clock.java
+++ src/main/java/clock/Clock.java
@@ -3,9 +3,6 @@ package clock;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
@@ -24,13 +21,14 @@ import javax.swing.plaf.FontUIResource;
 public class Clock extends JFrame {
 
 	private JPanel contentPane;
-	JLabel stateLable;
+	private volatile JLabel stateLable;
 	private static int sitMinites = 30;
 	private static int standMinites = 20;
 	private static final int fontSize = 60;
 
 	private volatile boolean sitFirst = true;
-	long startTime;
+	private volatile long startTime;
+	private volatile boolean isSitting;
 
 	Thread thread;
 
@@ -70,7 +68,7 @@ public class Clock extends JFrame {
 			}
 		});
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 200);
+		setBounds(1470, 100, 450, 200);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
@@ -102,7 +100,7 @@ public class Clock extends JFrame {
 		JPanel panel = new JPanel();
 		contentPane.add(panel, BorderLayout.CENTER);
 
-		stateLable = new JLabel("坐站状态");
+		stateLable = new JLabel();
 		stateLable.setFont(new Font("", Font.BOLD, fontSize)); // 设置文字的字体及大小
 		panel.add(stateLable);
 
@@ -122,19 +120,31 @@ public class Clock extends JFrame {
 			}
 		});
 		thread.start();
+		new Thread(() -> {
+			while (true) {
+				try {
+					status();
+					Thread.sleep(1000);
+				} catch (InterruptedException e1) {
+//					e1.printStackTrace();
+				}
+			}
+		}).start();
 	}
 
 	private void startSitTiming() throws InterruptedException {
-		stateLable.setText("坐着");
+//		stateLable.setText("坐着");
+		isSitting = true;
+		startTime = System.currentTimeMillis();
 		Thread.sleep(sitMinites * 60 * 1000);
 		alert("坐了" + sitMinites + "分钟了，该站一会了");
-		startTime = System.currentTimeMillis();
 	}
 	private void startStandTiming() throws InterruptedException {
-		stateLable.setText("站着");
+		isSitting = false;
+		startTime = System.currentTimeMillis();
+//		stateLable.setText("站着");
 		Thread.sleep(standMinites * 60 * 1000);
 		alert("站了" + standMinites + "分钟了，该坐一会了");
-		startTime = System.currentTimeMillis();
 	}
 	private void alert(String msg) {
 		JOptionPane message = new JOptionPane();
@@ -143,43 +153,17 @@ public class Clock extends JFrame {
 		message.showMessageDialog(null, msg);
 	}
 
-	public class TimePane extends JPanel {
-
-		@Override
-		public void paintComponent(Graphics g) {
-			super.paintComponent(g);
-			Graphics2D g2d = (Graphics2D) g;
-
-			// 抗锯齿
-			RenderingHints hints = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-			hints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-			g2d.addRenderingHints(hints);
-
-			EventQueue.invokeLater(new Runnable() {
-
-				@Override
-				public void run() {
-					drawTime(g2d);
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			});
-
+	private void status() {
+		if (startTime == 0) {
+			return;
 		}
-
-		private void drawTime(Graphics2D g2d) {
-			int time = (int) ((System.currentTimeMillis() - startTime) / 1000);
-			int m = time / 60;
-			int s = time % 60;
-			String ms = m == 0 ? "00" : m < 10 ? "0" + m : m + "";
-			String ss = s == 0 ? "00" : s < 10 ? "0" + s : s + "";
-			String text = ms + ":" + ss;
-			GameVisHelper.drawText(g2d, text, 50, 50, GameVisHelper.Green);
-		}
+		int time = (int) ((System.currentTimeMillis() - startTime) / 1000);
+		int m = time / 60;
+		int s = time % 60;
+		String ms = m == 0 ? "00" : m < 10 ? "0" + m : m + "";
+		String ss = s == 0 ? "00" : s < 10 ? "0" + s : s + "";
+		String text = ms + ":" + ss;
+		String status = (isSitting ? "坐着 " : "站着 ") + text;
+		stateLable.setText(status);
 	}
-
 }
